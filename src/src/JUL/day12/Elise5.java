6cdd4228ba047d98b8720382104a5e7f71c972b1
sinchangho <vkdghckdh@gmail.com>
2024-07-12 15:25:30 +0900

    엘리스 코딩챌리지 5문제 2차 풀이(성공)

--- src/src/JUL/day12/Elise5.java
+++ src/src/JUL/day12/Elise5.java
@@ -2,72 +2,55 @@ package JUL.day12;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
-
+import java.util.*;
+//5
+//0 1 2 3 3 4 4 5 5 6 6 7 7 8 9 10 10 11 12 13 13 14 14 15 15 16 16 17 17 18 19 20
 public class Elise5 {
-    static HashMap<Integer, Integer> combiMap = new HashMap<>();
-
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         // 이미 수열안에 원소들도 포함되어있음
         // 작은 수 부터 조합으로 만들수 있는 숫자가 있으면, 그 숫자 제외
         // 남은 수를 비교하여, 모두 합한 값이  max값을 넘어가면 안댐. (애초에 작은 수부터 봤기때문에, 같아지면 순환끝임)
         int n = Integer.parseInt(br.readLine());
-        int pow = (int)Math.pow(2 , n);
+        int pow = (int) Math.pow(2, n);
 
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0; i< pow; i++){
+        for (int i = 0; i < pow; i++) {
             pq.add(Integer.parseInt(st.nextToken()));
         }
-
-        ArrayList<Integer> result = new ArrayList<>();
-
-        while(!pq.isEmpty()){
-            int el =  pq.poll();
-            if(el == 0) continue;
-            // 조합으로 나온 수중에 있으면 제외
-            if(combiMap.containsKey(el)){
-                int cnt = combiMap.get(el) -1 ;
-                combiMap.put(el, cnt);
-                if(cnt ==0) combiMap.remove(el);
+        HashMap<Integer, Integer> makeMap = new HashMap<>(); // 만든 조합 저장
+        HashMap<Integer, Integer> map = new HashMap<>(); // 기존에 주어지는 값 저장
+        ArrayList<Integer> result = new ArrayList<>(); // 원소 저장하는 곳
+        ArrayList<Integer> combiList = new ArrayList<>(); // 조합 저장하는 곳
+        combiList.add(0);
+        makeMap.put(0, 1);
+        map.put(0, 1);
+        while (!pq.isEmpty()) {
+            int el = pq.poll();
+            if (el == 0) continue;
+            map.put(el, map.getOrDefault(el, 0) + 1);
+            // 조합으로 만든 값들이 주어지는 값을 포함하면 카운트하고 패스
+            if (makeMap.containsKey(el) && makeMap.get(el) >= map.get(el)) {
                 continue;
             }
             // 없으면 추가
             result.add(el);
-            if(result.size() == n){
+            if (result.size() == n) {
                 break;
             }
-            // 조합 추가하기
-            for(int target=2; target<= result.size(); target++){
-                boolean[] visited = new boolean[target];
-                combi(result, target, 0, 0, visited);
+            int size = combiList.size();
+            for (int i = 0; i < size; i++) {
+                int addCombi = combiList.get(i) + el;
+                combiList.add(addCombi);
+                makeMap.put(addCombi, makeMap.getOrDefault(addCombi, 0) + 1);
             }
-
         }
         StringBuilder sb = new StringBuilder();
-        for(int a : result){
+        for (int a : result) {
             sb.append(a).append(" ");
         }
 
         System.out.println(sb.toString());
-    }
-
-    private static void combi(ArrayList<Integer> list,int target, int cnt, int sum, boolean[] visited){
-        if(cnt == target){ // 2개부터 ~ n개 까지
-            if(combiMap.containsKey(sum)) return;
-            combiMap.put(sum, 1);
-            return;
-        }
-        for(int i=0; i < list.size(); i++){
-            if(visited[i]) continue;
-            visited[i] = true;
-            combi(list, target, cnt+1, sum + list.get(i), visited);
-            visited[i] = false;
-        }
-
     }
 }
